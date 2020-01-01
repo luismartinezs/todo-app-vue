52dd2cc1da7b17a8433f26150066b5f08d42f614
Lluís Martínez Suárez <luismartinezwebdev@gmail.com>
2020-01-01 21:11:26 +0100

    Static header and favicons

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,5 @@ export default {
   font-family: $font-family-base;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,43 @@
 <template>
-    <header>
-        Header
-    </header>
+  <header>
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-brand href="#">
+        <img class="header__logo" :src="logo" alt="To Do App logo" />
+        <span class="ml-3">ToDo List</span>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto mr-3">
+            <!-- Using 'button-content' slot -->
+            <span class="header__login">Login</span>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
 </template>
 
 <script>
+import logo from '@/assets/logo.png'
+
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+  data () {
+    return {
+      logo
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+
+.header__logo {
+  width: 3rem;
+  height: auto;
+}
+.header__login {
+    color: $white;
+}
+</style>
